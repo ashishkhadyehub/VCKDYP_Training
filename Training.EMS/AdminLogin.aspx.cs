9af9c7eadac6ed01d7b19fa5b8dea46920e65f68
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-06-22 20:01:32 +0530

    dotnet project day 2

---
+++ Training.EMS/AdminLogin.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Training.EMS
+{
+    public partial class AdminLogin : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(txtEmail.Text=="admin@gmail.com" && txtPassword.Text=="admin")
+            {
+                Session["Admin"] = "True";
+                Response.Redirect("EmployeeList.aspx");
+            }
+            else
+            {
+                this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Invalid Credentials..!','Please try again','error');", true);
+            }
+        }
+    }
+}
---
+++ Training.EMS/EmployeeProfile.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Training.EMS
+{
+    public partial class EmployeeProfile : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           if(Request.QueryString["id"]!=null)
+            {
+                getData();
+            }
+           else
+            {
+                Response.Redirect("EmployeeList.aspx");
+            }
+        }
+
+        protected void getData()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from EmpRegister where Srno=@EmployeeId", con);
+            cmd.Parameters.AddWithValue("@EmployeeId", Request.QueryString["id"]);
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            if (sdr.HasRows)
+            {
+                sdr.Read();
+                txtName.Text = sdr.GetValue(1).ToString();
+                txtEmail.Text = sdr.GetValue(2).ToString();
+                txtContact.Text = sdr.GetValue(3).ToString();
+                ddldept.SelectedItem.Text = sdr.GetValue(4).ToString();
+
+            }
+        }
+    }
+}
